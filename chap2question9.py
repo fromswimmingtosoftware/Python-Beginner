e5abcf37ebcfe02afd71de12b776ae8247fa8bfb
fromswimmingtosoftware <h2obrain@yahoo.com>
2016-03-01 18:04:02 -0500

    Added files via upload

---
+++ chap2question9.py
@@ -0,0 +1,14 @@
+# Chapter 2 Question 9
+
+print ('Please enter a number from 1 to 10.')
+spam = input()
+spam = int(spam)
+
+if spam == 1:
+    print('Hello')
+elif spam == 2:
+    print('Howdy')
+else:
+    print('Greetings')
+
+    
---
+++ elifexample.py
@@ -0,0 +1,16 @@
+print('What is your name?')
+name = input()
+print('What is your age?')
+age = input()
+age = int(age)
+
+
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, granny.')
+    
